5fce24b24af8186304b5c117d30498841b49b9b1
DavidMatevosyan21D <113889057+DavidMatevosyan21D@users.noreply.github.com>
2022-10-24 18:42:02 +0300

    Add files via upload

---
+++ Matevosyan_4_107/Matevosyan_4_107/Program.cs
@@ -0,0 +1,51 @@
+﻿
+Console.WriteLine("Введите номер масти карты (1-пики, 2-крести, 3-бубни, 4-черви ");
+int cardNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер значения карты");
+int cardMeaning = int.Parse(Console.ReadLine());
+
+switch (cardMeaning)
+{
+    case 6:
+        Console.WriteLine("Шестерка");
+        break;
+    case 7:
+        Console.WriteLine("Семерка");
+        break;
+    case 8:
+        Console.WriteLine("Восьмерка");
+        break;
+    case 9:
+        Console.WriteLine("Девятка");
+        break;
+    case 10:
+        Console.WriteLine("Десятка");
+        break;
+    case 11:
+        Console.WriteLine("Валет");
+        break;
+    case 12:
+        Console.WriteLine("Дама");
+        break;
+    case 13:
+        Console.WriteLine("Король");
+        break;
+    case 14:
+        Console.WriteLine("Туз");
+        break;
+}
+switch (cardNumber)
+{
+    case 1:
+        Console.WriteLine(" пики");
+        break;
+    case 2:
+        Console.WriteLine(" крести");
+        break;
+    case 3:
+        Console.WriteLine(" бубни");
+        break;
+    case 4:
+        Console.WriteLine(" червей");
+        break;
+}
---
+++ Matevosyan_4_114/Matevosyan_4_114/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите год: ");
+int year = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер месяца: ");
+int mounthNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите день: ");
+int day = int.Parse(Console.ReadLine());
+Console.WriteLine($"Дата предыдущего дня: {day - 1}.{mounthNumber}.{year}");
+Console.WriteLine($"Дата следующего дня: {day + 1}.{mounthNumber}.{year}");
+if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+{
+    Console.WriteLine("Год високосный");
+}
+else
+{
+    Console.WriteLine("Год не високосный");
+}
